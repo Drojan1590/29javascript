51a659309f1f50325254ffd3c8993d9d22cd7885
Alejandro Sanchez <alexchicha23@gmail.com>
2023-10-12 21:49:10 -0600

    Mi primer codigo en javascript

---
+++ s1variablesFunciones/main.js
@@ -0,0 +1,15 @@
+console.log("Hola Koders")
+
+let a = 2
+let b = 5
+let resultado1 = a + b
+let resultado2 = a - b
+let resultado3 = a * b
+let resultado4 = a / b
+let resultado5 = a % b
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
